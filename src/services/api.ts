962b927773f00a03052ef876d3c7c1511d9c8d8d
Kujikeo <36707552+Kujikeo@users.noreply.github.com>
2021-04-29 18:23:50 -0300

    arrumado troca

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let api = axios.create({
-  baseURL: "http://192.168.0.2:3333",
+  baseURL: "https://inova-backend-dev.azurewebsites.net/",
 
   headers: {
     "Access-Control-Allow-Origin": "*",
